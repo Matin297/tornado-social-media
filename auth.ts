10239bfa0ccea954cf40d41babad402dcfcb2ef7
Matin Mir <asmamir297@gmail.com>
2024-10-16 15:30:39 +0330

    configure next-auth and add prisma adapter

---
+++ auth.ts
@@ -0,0 +1,9 @@
+import db from "@/prisma/client";
+import NextAuth from "next-auth";
+import GitHub from "next-auth/providers/github";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+
+export const { handlers, signIn, signOut, auth } = NextAuth({
+  providers: [GitHub],
+  adapter: PrismaAdapter(db),
+});
