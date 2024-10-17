3557ca2852481d41518d21f0f276d3320d2f3d25
Matin Mir <asmamir297@gmail.com>
2024-10-17 15:39:54 +0330

    implement search section for navbar

--- app/(home)/actions.ts
+++ app/(home)/actions.ts
@@ -1,7 +1,15 @@
 "use server";
 
+import { redirect } from "next/navigation";
 import { signOut as AuthSignOut } from "@/auth";
 
 export async function signOut() {
   await AuthSignOut({ redirectTo: "/auth" });
+}
+
+export async function search(formData: FormData) {
+  const query = (formData.get("q") as string)?.trim();
+  if (query) {
+    redirect("/search?q=" + query);
+  }
 }
