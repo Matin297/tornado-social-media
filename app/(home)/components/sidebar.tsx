7c0e2e67e378f205b8d06599a5fc38beab88936b
Matin Mir <asmamir297@gmail.com>
2024-10-18 16:45:26 +0330

    implement sidebar navigation

---
+++ app/(home)/components/sidebar.tsx
@@ -0,0 +1,58 @@
+"use client";
+
+import Link from "next/link";
+import { cn } from "@/lib/utils";
+import { usePathname } from "next/navigation";
+import { Button } from "@/components/ui/button";
+import { Home, Bell, Mail, Bookmark } from "lucide-react";
+
+const LINKS = [
+  {
+    href: "/",
+    title: "Home",
+    icon: <Home />,
+  },
+  {
+    href: "/notifications",
+    title: "Notifications",
+    icon: <Bell />,
+  },
+  {
+    href: "/messages",
+    title: "Messages",
+    icon: <Mail />,
+  },
+  {
+    href: "/bookmarks",
+    title: "Bookmarks",
+    icon: <Bookmark />,
+  },
+];
+
+interface SidebarProps {
+  className?: string;
+}
+
+export default function Sidebar({ className }: SidebarProps) {
+  const pathname = usePathname();
+
+  return (
+    <nav className={cn("rounded-md border bg-card shadow-sm", className)}>
+      {LINKS.map(({ href, title, icon }) => (
+        <Button
+          asChild
+          variant="ghost"
+          className={cn(
+            "w-full rounded-none sm:justify-start sm:rounded-md",
+            pathname === href && "bg-accent text-accent-foreground",
+          )}
+        >
+          <Link href={href}>
+            {icon}
+            <span className="hidden lg:inline-block">{title}</span>
+          </Link>
+        </Button>
+      ))}
+    </nav>
+  );
+}
