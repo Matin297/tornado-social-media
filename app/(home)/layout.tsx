d7edef06873b7fabf4ec1bdd865989eb4391dfd6
Matin Mir <asmamir297@gmail.com>
2024-10-18 16:45:57 +0330

    adjust home layout with sidebar navigation based on viewport

--- app/(home)/layout.tsx
+++ app/(home)/layout.tsx
@@ -1,12 +1,19 @@
-import { PropsWithChildren } from "react";
 import Navbar from "./components/navbar";
+import { PropsWithChildren } from "react";
+import Sidebar from "./components/sidebar";
 
 export default function HomeLayout({ children }: PropsWithChildren) {
   return (
     <>
       <Navbar />
-      <main>Here goes main: {children}</main>
-      <footer>Here goes footer</footer>
+      <main className="flex p-4 sm:gap-4">
+        <aside>
+          <Sidebar className="sticky top-2 hidden flex-col gap-1 p-2 sm:flex" />
+        </aside>
+        <section>Here goes main: {children}</section>
+      </main>
+      <footer className="p-4">Here goes footer</footer>
+      <Sidebar className="sticky bottom-0 flex rounded-none border-x-0 sm:hidden" />
     </>
   );
 }
