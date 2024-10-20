560e51163671c9340cc556f738ba2f1b7d15c9cc
Matin Mir <asmamir297@gmail.com>
2024-10-20 14:21:58 +0330

    adjust home layout and include toaster

--- app/(home)/layout.tsx
+++ app/(home)/layout.tsx
@@ -1,6 +1,7 @@
 import Navbar from "./components/navbar";
 import { PropsWithChildren } from "react";
 import Sidebar from "./components/sidebar";
+import { Toaster } from "@/components/ui/toaster";
 
 export default function HomeLayout({ children }: PropsWithChildren) {
   return (
@@ -10,10 +11,11 @@ export default function HomeLayout({ children }: PropsWithChildren) {
         <aside>
           <Sidebar className="sticky top-2 hidden flex-col gap-1 p-2 sm:flex" />
         </aside>
-        <section>Here goes main: {children}</section>
+        <section className="grow space-y-4">{children}</section>
       </main>
       <footer className="p-4">Here goes footer</footer>
       <Sidebar className="sticky bottom-0 flex rounded-none border-x-0 sm:hidden" />
+      <Toaster />
     </>
   );
 }
