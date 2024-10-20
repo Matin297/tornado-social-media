74d2ee5f094d9031eedcacd0d57708bf9ee5f607
Matin Mir <asmamir297@gmail.com>
2024-10-20 14:18:24 +0330

    implement merge refs utility

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,21 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { MutableRefObject, RefCallback, RefObject } from "react";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function mergeRefs<Instance>(
+  ...refs: (null | MutableRefObject<Instance> | RefCallback<Instance>)[]
+) {
+  return (instance: Instance) => {
+    refs.forEach((ref) => {
+      if (typeof ref === "function") {
+        ref(instance);
+      } else if (ref !== null) {
+        ref.current = instance;
+      }
+    });
+  };
 }
