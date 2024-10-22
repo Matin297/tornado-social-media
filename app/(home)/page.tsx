dc2a7a9f9adba34996002a979e23cf24b388b8c6
Matin Mir <asmamir297@gmail.com>
2024-10-22 15:15:53 +0330

    stream post list data in home page

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,6 +1,9 @@
 import { auth } from "@/auth";
+import { Suspense } from "react";
 import { redirect } from "next/navigation";
+import PostList from "./components/post-list";
 import CreatePostForm from "./components/create-post-form";
+import PostListSkeleton from "./components/post-list-skeleton";
 
 export default async function Home() {
   const session = await auth();
@@ -9,5 +12,12 @@ export default async function Home() {
     redirect("/auth");
   }
 
-  return <CreatePostForm />;
+  return (
+    <>
+      <CreatePostForm />
+      <Suspense fallback={<PostListSkeleton />}>
+        <PostList />
+      </Suspense>
+    </>
+  );
 }
