8c1a8885b83e0e353fc5f7f19d6343e8c061a481
Matin Mir <asmamir297@gmail.com>
2024-10-28 15:54:35 +0330

    adjust post list and home page to include pagination component

--- app/(home)/components/post-list.tsx
+++ app/(home)/components/post-list.tsx
@@ -1,6 +1,4 @@
 import Link from "next/link";
-import { formatDistanceToNow } from "date-fns";
-import { fetchPosts } from "../data";
 import {
   Card,
   CardTitle,
@@ -8,51 +6,64 @@ import {
   CardHeader,
   CardContent,
 } from "@/components/ui/card";
+import { formatDistanceToNow } from "date-fns";
+import Pagination from "@/components/pagination";
+import { fetchPosts, fetchTotalPostPages } from "../data";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
-export default async function PostList() {
-  const posts = await fetchPosts();
+interface PostListProps {
+  page: number;
+}
+
+export default async function PostList({ page }: PostListProps) {
+  const [totalPages, posts] = await Promise.all([
+    fetchTotalPostPages(),
+    fetchPosts({ page }),
+  ]);
 
   return (
-    <ul className="space-y-4">
-      {posts.map((post) => (
-        <li key={post.id}>
-          <article>
-            <Card>
-              <CardHeader>
-                <CardTitle>
-                  <Link
-                    href={`/users/${post.userId}`}
-                    className="flex items-center gap-2"
-                  >
-                    <Avatar>
-                      <AvatarImage
-                        src={post?.user?.image ?? ""}
-                        alt={post.user.name ?? "John Doe"}
-                      />
-                      <AvatarFallback>
-                        {post?.user?.name?.[0] ?? "JD"}
-                      </AvatarFallback>
-                    </Avatar>
-                    <div className="text-sm">
-                      <p>{post.user.name}</p>
-                      <p className="font-normal">{post.user.email}</p>
-                    </div>
-                  </Link>
-                </CardTitle>
-              </CardHeader>
-              <CardContent>
-                <p>{post.content}</p>
-              </CardContent>
-              <CardFooter>
-                <p className="text-sm">
-                  {formatDistanceToNow(post.updatedAt, { addSuffix: true })}
-                </p>
-              </CardFooter>
-            </Card>
-          </article>
-        </li>
-      ))}
-    </ul>
+    <>
+      <ul className="space-y-4">
+        {posts.map((post) => (
+          <li key={post.id}>
+            <article>
+              <Card>
+                <CardHeader>
+                  <CardTitle>
+                    <Link
+                      href={`/users/${post.userId}`}
+                      className="flex items-center gap-2"
+                    >
+                      <Avatar>
+                        <AvatarImage
+                          src={post?.user?.image ?? ""}
+                          alt={post.user.name ?? "John Doe"}
+                        />
+                        <AvatarFallback>
+                          {post?.user?.name?.[0] ?? "JD"}
+                        </AvatarFallback>
+                      </Avatar>
+                      <div className="text-sm">
+                        <p>{post.user.name}</p>
+                        <p className="font-normal">{post.user.email}</p>
+                      </div>
+                    </Link>
+                  </CardTitle>
+                </CardHeader>
+                <CardContent>
+                  <p>{post.content}</p>
+                </CardContent>
+                <CardFooter>
+                  <p className="text-sm">
+                    {formatDistanceToNow(post.updatedAt, { addSuffix: true })}
+                  </p>
+                </CardFooter>
+              </Card>
+            </article>
+          </li>
+        ))}
+      </ul>
+      <Pagination totalPages={totalPages} />
+    </>
   );
 }
