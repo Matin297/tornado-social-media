c04089df1038eb432eb51d60f3a46ee7336a8f2d
Matin Mir <asmamir297@gmail.com>
2024-10-28 15:48:43 +0330

    add fetch total pages and support pagination for posts data functions

--- app/(home)/data.ts
+++ app/(home)/data.ts
@@ -1,6 +1,19 @@
 import db from "@/prisma/client";
 
-export async function fetchPosts() {
+const POSTS_PER_PAGE = 5;
+export async function fetchTotalPostPages() {
+  try {
+    const count = await db.post.count();
+    return Math.ceil(count / POSTS_PER_PAGE);
+  } catch (error) {
+    if (error instanceof Error) {
+      throw error;
+    }
+    throw new Error("Failed to fetch total pages for posts!");
+  }
+}
+
+export async function fetchPosts({ page }: { page: number }) {
   try {
     const posts = await db.post.findMany({
       orderBy: {
@@ -15,6 +28,8 @@ export async function fetchPosts() {
           },
         },
       },
+      take: POSTS_PER_PAGE,
+      skip: (page - 1) * POSTS_PER_PAGE,
     });
     return posts;
   } catch (error) {
