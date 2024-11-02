4f7b92186c232ccc1ad86f889449d3120eef4567
Matin Mir <asmamir297@gmail.com>
2024-11-02 16:57:31 +0330

    move theme and session providers to root layout

--- providers/theme-provider.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-
-import { ThemeProvider as NextThemeProvider } from "next-themes";
-import type { ThemeProviderProps } from "next-themes/dist/types";
-
-export default function Providers(props: ThemeProviderProps) {
-  return <NextThemeProvider {...props} />;
-}
