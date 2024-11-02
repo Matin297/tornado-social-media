306a2f76b872dcba1d6e50e9ca029e9b5ed13c9c
Matin Mir <asmamir297@gmail.com>
2024-11-02 17:02:02 +0330

    move session provider to home section layout

--- app/(home)/layout.tsx
+++ app/(home)/layout.tsx
@@ -2,10 +2,11 @@ import Navbar from "./components/navbar";
 import { PropsWithChildren } from "react";
 import Sidebar from "./components/sidebar";
 import { Toaster } from "@/components/ui/toaster";
+import { SessionProvider } from "next-auth/react";
 
 export default function HomeLayout({ children }: PropsWithChildren) {
   return (
-    <>
+    <SessionProvider>
       <Navbar />
       <main className="flex p-4 sm:gap-4">
         <aside>
@@ -16,6 +17,6 @@ export default function HomeLayout({ children }: PropsWithChildren) {
       <footer className="p-4">Here goes footer</footer>
       <Sidebar className="sticky bottom-0 flex rounded-none border-x-0 sm:hidden" />
       <Toaster />
-    </>
+    </SessionProvider>
   );
 }
