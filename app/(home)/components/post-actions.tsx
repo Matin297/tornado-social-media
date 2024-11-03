16528e43e15fe179484dacf7c30a3e713b0c5494
Matin Mir <asmamir297@gmail.com>
2024-11-03 13:48:07 +0330

    fix the loading issue for delete action on post items

--- app/(home)/components/post-actions.tsx
+++ app/(home)/components/post-actions.tsx
@@ -42,30 +42,33 @@ export default function PostActions({ postId, postUserId }: PostActionsProps) {
     }
   }
 
-  if (data?.user?.id !== postUserId) return null;
-  if (status === "loading") return <Skeleton className="h-9 w-20" />;
-  return (
-    <Dialog open={open}>
-      <Button onClick={() => setOpen(true)} variant="destructive">
-        Delete
-      </Button>
-      <DialogContent>
-        <DialogHeader>
-          <DialogTitle>Delete Post</DialogTitle>
-          <DialogDescription>
-            Are you sure you want to delete this post?
-          </DialogDescription>
-        </DialogHeader>
-        <DialogFooter>
-          <form action={deletePost}>
-            <input type="hidden" name="postID" value={postId} />
-            <SubmitButton variant="destructive">Confirm</SubmitButton>
-          </form>
-          <Button onClick={() => setOpen(false)} variant="secondary">
-            Cancel
-          </Button>
-        </DialogFooter>
-      </DialogContent>
-    </Dialog>
-  );
+  if (status === "loading") return <Skeleton className="h-9 w-16" />;
+
+  if (status === "authenticated" && data.user?.id === postUserId)
+    return (
+      <Dialog open={open}>
+        <Button onClick={() => setOpen(true)} variant="destructive">
+          Delete
+        </Button>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>Delete Post</DialogTitle>
+            <DialogDescription>
+              Are you sure you want to delete this post?
+            </DialogDescription>
+          </DialogHeader>
+          <DialogFooter>
+            <form action={deletePost}>
+              <input type="hidden" name="postID" value={postId} />
+              <SubmitButton variant="destructive">Confirm</SubmitButton>
+            </form>
+            <Button onClick={() => setOpen(false)} variant="secondary">
+              Cancel
+            </Button>
+          </DialogFooter>
+        </DialogContent>
+      </Dialog>
+    );
+
+  return null;
 }
