8e6b79951aac069e04b2a76eaaa6c7c2665364e2
Matin Mir <asmamir297@gmail.com>
2024-11-13 21:10:43 +0330

    use mutation for deleting a post

--- app/(home)/components/post-actions.tsx
+++ app/(home)/components/post-actions.tsx
@@ -8,13 +8,15 @@ import {
   DialogContent,
   DialogDescription,
 } from "@/components/ui/dialog";
+import { deletePostById } from "../data";
 import { useToast } from "@/hooks/use-toast";
 import { useSession } from "next-auth/react";
 import { Button } from "@/components/ui/button";
+import { useSearchParams } from "next/navigation";
 import { PropsWithChildren, useState } from "react";
 import { Skeleton } from "@/components/ui/skeleton";
-import SubmitButton from "@/components/submit-button";
-import { deletePost as deletePostAction } from "../actions";
+import LoadingButton from "@/components/loading-button";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 
 interface PostActionsProps extends PropsWithChildren {
   postId: string;
@@ -22,24 +24,35 @@ interface PostActionsProps extends PropsWithChildren {
 }
 
 export default function PostActions({ postId, postUserId }: PostActionsProps) {
+  const queryClient = useQueryClient();
+
   const { toast } = useToast();
   const { data, status } = useSession();
+
   const [open, setOpen] = useState(false);
 
-  async function deletePost(formData: FormData) {
-    const result = await deletePostAction(formData);
+  const searchParams = useSearchParams();
+  const page = searchParams.get("page") ?? "1";
 
-    if (result) {
-      toast({
-        title: result.error,
-        variant: "destructive",
-      });
-    } else {
-      toast({
-        title: "Post deleted successfully",
-      });
+  const deleteMutation = useMutation({
+    mutationFn: () => deletePostById(postId),
+    onSuccess: () => {
       setOpen(false);
-    }
+      toast({
+        title: "Post delete successfully!",
+      });
+      queryClient.invalidateQueries({ queryKey: ["posts", page] });
+    },
+    onError: (error) => {
+      toast({
+        variant: "destructive",
+        title: error.message,
+      });
+    },
+  });
+
+  function handlePostDeletion() {
+    deleteMutation.mutate();
   }
 
   if (status === "loading") return <Skeleton className="h-9 w-16" />;
@@ -58,10 +71,12 @@ export default function PostActions({ postId, postUserId }: PostActionsProps) {
             </DialogDescription>
           </DialogHeader>
           <DialogFooter>
-            <form action={deletePost}>
-              <input type="hidden" name="postID" value={postId} />
-              <SubmitButton variant="destructive">Confirm</SubmitButton>
-            </form>
+            <LoadingButton
+              onClick={handlePostDeletion}
+              pending={deleteMutation.isPending}
+            >
+              Delete
+            </LoadingButton>
             <Button onClick={() => setOpen(false)} variant="secondary">
               Cancel
             </Button>
