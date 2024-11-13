2b3491a1aa17c252cd1207ea8bf1b4aaeed5785b
Matin Mir <asmamir297@gmail.com>
2024-11-13 21:08:15 +0330

    create data functions for fetching, deleting and creating a post

--- app/(home)/data.ts
+++ app/(home)/data.ts
@@ -1,41 +1,60 @@
-import db from "@/prisma/client";
+import { Post } from "@prisma/client";
+import { PostWithUser } from "@/prisma/payloads";
+import { CreatePostSchema } from "@/validations/posts";
 
-const POSTS_PER_PAGE = 5;
-export async function fetchTotalPostPages() {
-  try {
-    const count = await db.post.count();
-    return Math.ceil(count / POSTS_PER_PAGE);
-  } catch (error) {
-    if (error instanceof Error) {
-      throw error;
-    }
-    throw new Error("Failed to fetch total pages for posts!");
-  }
+interface FilterOptions {
+  page?: string | null;
 }
-
-export async function fetchPosts({ page }: { page: number }) {
+export async function fetchPosts({ page }: FilterOptions) {
   try {
-    const posts = await db.post.findMany({
-      orderBy: {
-        updatedAt: "desc",
-      },
-      include: {
-        user: {
-          select: {
-            name: true,
-            image: true,
-            email: true,
-          },
-        },
-      },
-      take: POSTS_PER_PAGE,
-      skip: (page - 1) * POSTS_PER_PAGE,
-    });
-    return posts;
+    const result = await fetch(`/api/posts/?page=${page ?? 1}`);
+    const data = await result.json();
+    if (!result.ok) {
+      throw new Error(data.message);
+    }
+    return data as {
+      count: number;
+      posts: PostWithUser[];
+    };
   } catch (error) {
     if (error instanceof Error) {
       throw error;
     }
     throw new Error("Cannot fetch posts!");
   }
+}
+
+export async function deletePostById(id: string) {
+  try {
+    const result = await fetch(`/api/posts/${id}`, {
+      method: "post",
+    });
+    const data = await result.json();
+    if (!result.ok) {
+      throw new Error(data.message);
+    }
+    return data as Post;
+  } catch (error) {
+    if (error instanceof Error) {
+      throw error;
+    }
+    throw new Error("Cannot fetch posts!");
+  }
+}
+
+// We will let useMutation catch the errors when thrown
+export async function createPost(formData: FormData) {
+  CreatePostSchema.parse(Object.fromEntries(formData.entries()));
+
+  const result = await fetch("/api/posts", {
+    method: "POST",
+    body: formData,
+  });
+  const data = await result.json();
+
+  if (!result.ok) {
+    throw data;
+  }
+
+  return data as PostWithUser;
 }
