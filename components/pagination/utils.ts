a331295c3625e76917b68b8dcee136493b295bbb
Matin Mir <asmamir297@gmail.com>
2024-11-13 20:36:15 +0330

    update pagination logic to adapt to less than 2 pages

--- components/pagination/utils.ts
+++ components/pagination/utils.ts
@@ -1,6 +1,10 @@
 export const generatePagination = (currentPage: number, totalPages: number) => {
   // If the total number of pages is 7 or less,
   // display all pages without any ellipsis.
+  if (totalPages < 2) {
+    return [];
+  }
+
   if (totalPages <= 7) {
     return Array.from({ length: totalPages }, (_, i) => i + 1);
   }
