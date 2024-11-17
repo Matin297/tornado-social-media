ad8e63c17bbe0f3ecd2cd8beee1aae3820090a5c
Matin Mir <asmamir297@gmail.com>
2024-11-17 18:09:50 +0330

    clear react query cache before logout server action

--- app/(home)/components/logout.tsx
+++ app/(home)/components/logout.tsx
@@ -1,14 +1,25 @@
+"use client";
+
 import { cn } from "@/lib/utils";
 import { signOut } from "../actions";
 import SubmitButton from "@/components/submit-button";
+import { useQueryClient } from "@tanstack/react-query";
 
 interface LogoutProps {
   className?: string;
 }
 
 export default function Logout({ className }: LogoutProps) {
+  const queryClient = useQueryClient();
+
   return (
-    <form action={signOut} className={cn(className)}>
+    <form
+      action={() => {
+        queryClient.clear();
+        signOut();
+      }}
+      className={cn(className)}
+    >
       <SubmitButton className="w-full" variant="ghost" size="sm">
         SignOut
       </SubmitButton>
