87cc3a594ee889fb1b82f851fd3037b663cd3afb
Matin Mir <asmamir297@gmail.com>
2024-11-22 17:52:00 +0330

    add toggle follow status server action

--- app/(home)/actions.ts
+++ app/(home)/actions.ts
@@ -1,7 +1,9 @@
 "use server";
 
+import db from "@/prisma/client";
 import { redirect } from "next/navigation";
-import { signOut as AuthSignOut } from "@/auth";
+import { revalidatePath } from "next/cache";
+import { auth, signOut as AuthSignOut } from "@/auth";
 
 export async function signOut() {
   await AuthSignOut({ redirectTo: "/auth" });
@@ -11,5 +13,55 @@ export async function search(formData: FormData) {
   const query = (formData.get("q") as string)?.trim();
   if (query) {
     redirect("/search?q=" + query);
+  }
+}
+
+interface ToggleFollowStatus {
+  followingId: string;
+}
+export async function toggleFollowStatus({ followingId }: ToggleFollowStatus) {
+  const session = await auth();
+
+  if (!session || !session.user?.id) {
+    return {
+      message: "Unauthenticated request!",
+    };
+  }
+
+  try {
+    const followRecord = await db.follow.findUnique({
+      where: {
+        followerId_followingId: {
+          followingId,
+          followerId: session.user.id,
+        },
+      },
+    });
+
+    if (followRecord) {
+      // un-follow
+      await db.follow.delete({
+        where: {
+          followerId_followingId: {
+            followingId,
+            followerId: session.user.id,
+          },
+        },
+      });
+    } else {
+      // follow
+      await db.follow.create({
+        data: {
+          followingId,
+          followerId: session.user.id,
+        },
+      });
+    }
+
+    revalidatePath("/");
+  } catch (error) {
+    return {
+      message: "Unable to toggle follow status for this user!",
+    };
   }
 }
