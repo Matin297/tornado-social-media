5fae77ba6b60c641919420ecabfb0875feaa336d
Matin Mir <asmamir297@gmail.com>
2024-11-22 17:53:52 +0330

    adjust homepage layout to display follow suggestions as an aside column

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -2,6 +2,7 @@ import { auth } from "@/auth";
 import { redirect } from "next/navigation";
 import PostList from "./components/post-list";
 import CreatePostForm from "./components/create-post-form";
+import FollowSuggestions from "./components/follow-suggestions";
 
 export default async function Home() {
   const session = await auth();
@@ -11,9 +12,14 @@ export default async function Home() {
   }
 
   return (
-    <>
-      <CreatePostForm />
-      <PostList />
-    </>
+    <main className="flex flex-row gap-4">
+      <aside className="hidden max-w-xs shrink-0 basis-1/3 lg:order-1 lg:block">
+        <FollowSuggestions />
+      </aside>
+      <section className="grow space-y-4">
+        <CreatePostForm />
+        <PostList />
+      </section>
+    </main>
   );
 }
